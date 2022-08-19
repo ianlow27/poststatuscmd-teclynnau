e1f6d515ef4067937c8f17e5b936520d2dd6a835
Ian Low <ianlow28@gmail.com>
2022-08-19 15:56:01 +0100

    Ymrwymiad cychwyniedig

---
+++ src/PoststatuscmdTeclynnau.php
@@ -0,0 +1,32 @@
+<?php
+/* ==============================================
+TO DO:
+======
+- details
+-------------------------------------------------
+DONE:
+=====
+# Changelog
+## 1.0.0 - yyyy-mm-dd
+### Changed
+- details
+### Fixed
+- details
+### Added
+- details
+================================================= */
+namespace Ianl28\PoststatuscmdTeclynnau;
+class PoststatuscmdTeclynnau{
+
+  //-------------------------------
+  public function __construct(){
+    echo "This is called from PoststatuscmdTeclynnau->__construct()\n";
+  }//endfunc
+  //-------------------------------
+  public function testrun(){
+    echo "This is called from PoststatuscmdTeclynnau->testrun()\n";
+  }//endfunc
+  //-------------------------------
+
+}//endclass 
+?>
